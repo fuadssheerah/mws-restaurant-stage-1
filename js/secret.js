ea80b4c4c8547f2f7a3e538a711617e2a3627a3f
Fuad Sheerah <57592040+fuadssheerah@users.noreply.github.com>
2020-03-09 21:59:47 +0300

    edit .gitignore

---
+++ js/secret.js
@@ -0,0 +1,6 @@
+//keeping my API token key secret
+class SECRET {
+  static get mapbox_key() {
+    return "pk.eyJ1IjoiZnVhZHNnZWVyYWgiLCJhIjoiY2s2Z2M4MGtnMHo0ZTNlbnZ2ZWxxaG51byJ9.2yK4lmxpPbM1uIMtjeWC6A";
+  }
+}
